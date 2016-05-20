6361ac98809187923f19dbe561f81d38354e6df4
qweru <sara01dilan@gmail.com>
2016-05-20 17:53:57 +0430

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,13 +1,15 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+15104173796", "TeleMoon🌙", "", ok_cb, false)
+send_contact(get_receiver(msg), "+15104173796", "TeleMoon", "(Send !share for contact)", ok_cb, false)
 end
 
 return {
 patterns = {
-"^[!/#]share$",
-"^share$"
+"^!share$",
+"^share$",
+"^Share$",
+"^/share$",
 
 },
 run = run
